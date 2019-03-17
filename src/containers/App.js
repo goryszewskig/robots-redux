cf967d3647407aacb150ff99af77f23eb5a9ce25
goryszewskig <greg.szewski@gmail.com>
2019-03-17 10:49:14 +0100

    Title to reflect usage of thunk middleware

--- src/containers/App.js
+++ src/containers/App.js
@@ -59,7 +59,7 @@ class App extends Component {
             <h1>Loading...</h1> :
          (
             <div className='tc'>
-                <h1 className='f1'>RoboFriends</h1>
+                <h1 className='f1'>RoboFriends using redux-thunk</h1>
                 <SearchBox searchChange={onSearchChange}/>
                 <Scroll>
                     <ErrorBoundry>
